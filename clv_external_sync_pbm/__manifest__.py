d843b468180cb6746fd980e93fcd21b1a2f65917
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-07-04 17:20:02 -0300

    [IMP] clv_external_sync_pbm

--- clv_external_sync_pbm/__manifest__.py
+++ clv_external_sync_pbm/__manifest__.py
@@ -1,27 +1,11 @@
 # -*- coding: utf-8 -*-
-###############################################################################
-#
 # Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-###############################################################################
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'External Sync (customizations for CLVhealth-PBM Solution)',
     'summary': 'External Sync Module customizations for CLVhealth-PBM Solution.',
-    'version': '3.0.0',
+    'version': '4.0.0',
     'author': 'Carlos Eduardo Vercelino - CLVsol',
     'category': 'Generic Modules/Others',
     'license': 'AGPL-3',
@@ -31,6 +15,7 @@
     ],
     'data': [
         'views/external_sync_menu_view.xml',
+        'data/external_sync.xml',
     ],
     'demo': [],
     'test': [],
