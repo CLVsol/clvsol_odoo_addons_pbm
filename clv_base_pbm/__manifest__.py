a750840e567d226c21191f31d9cfcdca58cd4f3d
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-07-04 17:18:17 -0300

    [IMP] clv_base_pbm

--- clv_base_pbm/__manifest__.py
+++ clv_base_pbm/__manifest__.py
@@ -1,40 +1,35 @@
 # -*- coding: utf-8 -*-
-###############################################################################
-#
-# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-###############################################################################
+# Copyright (C) 2017-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Base Module (customizations for CLVhealth-PBM Solution)',
     'summary': 'Base Module Module customizations for CLVhealth-PBM Solution.',
-    'version': '3.0.0',
+    'version': '4.0.0',
     'author': 'Carlos Eduardo Vercelino - CLVsol',
     'category': 'Generic Modules/Others',
     'license': 'AGPL-3',
     'website': 'https://github.com/CLVsol',
     'depends': [
         'clv_base',
+        'contacts',
+        'base_address_city',
     ],
     'data': [
+        'views/base_address_city_menu_view.xml',
         'views/base_menu_view.xml',
+        'views/referenciable_model_menu_view.xml',
+        'views/global_settings_menu_view.xml',
+        'views/mfile_menu_view.xml',
+        'views/community_menu_view.xml',
+        'views/health_menu_view.xml',
         'views/export_menu_view.xml',
-        'views/insurance_menu_view.xml',
-        'views/pharmacy_menu_view.xml',
+        'views/report_menu_view.xml',
+        'views/external_sync_menu_view.xml',
+        'views/verification_menu_view.xml',
+        'views/processing_menu_view.xml',
         'data/annotation_seq.xml',
+        'data/global_settings_filestore.xml',
     ],
     'demo': [],
     'test': [],
