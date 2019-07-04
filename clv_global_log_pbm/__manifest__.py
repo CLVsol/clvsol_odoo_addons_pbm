3bf81a8a14a12cd7fee20b07aff6f30cf5060e93
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-07-04 17:20:28 -0300

    [ADD] clv_global_log_pbm

---
+++ clv_global_log_pbm/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Global Log (customizations for CLVhealth-PBM Solution)',
+    'summary': 'Global Log Module customizations for CLVhealth-PBM Solution.',
+    'version': '3.0.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'Generic Modules/Others',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/CLVsol',
+    'depends': [
+        'clv_base_pbm',
+        'clv_global_log',
+    ],
+    'data': [
+        'views/global_log_menu_view.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}
