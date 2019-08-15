bee2955fa976adfe175e684cbfccf33004336b14
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-08-15 15:25:49 -0300

    [ADD] clv_set_pbm

---
+++ clv_set_pbm/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Set (customizations for CLVhealth-PBM Solution)',
+    'summary': 'Set Module customizations for CLVhealth-PBM Solution.',
+    'version': '12.0.4.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'CLVsol Solutions',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/CLVsol',
+    'depends': [
+        'clv_set',
+    ],
+    'data': [
+        'views/set_menu_view.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}
